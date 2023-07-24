d32d718c485acdf58e6e72129290d4b9c8e4d1dc
DAILIDONIS Paulius <paulius.dailidonis@tamro.com>
2023-07-24 18:23:48 +0300

    adding crud operations

--- Training_for_last_task/crud_operations.py
+++ Training_for_last_task/crud_operations.py
@@ -1,3 +1,14 @@
+import data
+
+
+# 1. Add Part\n2. Delete Part\n3. Update Part\n4. Exit"
+OPERATIONS_DICT = { "Add part": 1,
+                    "Delete part": 2,
+                    "Update part": 3,
+                    "Exit": 4
+                }
+
+
 def add_info_to_dict(info_dict):
     name = input("Enter the name: ")
     if name not in info_dict:
@@ -82,37 +93,65 @@ def update_info_in_dict(info_dict):
 
 
 # Example usage:
-if __name__ == "__main__":
-    cpu_info = {
-        "name": "AMD",
-        "price": 162.66,
-        "brand": "AMD",
-        "speed": "3.7GHz",
-        "power_usage": "65W"
-    }
+def main():
+    # cpu_info = {
+    #     "name": "AMD",
+    #     "price": 162.66,
+    #     "brand": "AMD",
+    #     "speed": "3.7GHz",
+    #     "power_usage": "65W"
+    # }
 
-    cpu_cooler_info = {
-        "name": "be quiet! Dark Rock Pro 4",
-        "price": 89.90,
-        "noise_level": "12.8 - 24.3 dB",
-        "fan": "1500 RPM",
-        "color": "black"
-    }
+    # cpu_cooler_info = {
+    #     "name": "be quiet! Dark Rock Pro 4",
+    #     "price": 89.90,
+    #     "noise_level": "12.8 - 24.3 dB",
+    #     "fan": "1500 RPM",
+    #     "color": "black"
+    # }
 
     while True:
-        print("\n1. Add Part\n2. Delete Part\n3. Update Part\n4. Exit")
-        choice = input("Enter your choice (1/2/3/4): ")
+        print("Please choose which category you want to update: ")
+        component_name, component_dict = _user_selection_from_dict(data.master_info)
+        # print("\n1. cpu\n2. cpu cooler\n3. motherboard\n4. memory\n5.storage \n6. video card")
+        # print("\n1. Add Part\n2. Delete Part\n3. Update Part\n4. Exit")
+        # choice = input("Enter your choice (1/2/3/4): ")
 
-        if choice == "1":
-            add_info_to_dict(cpu_info)
-        elif choice == "2":
-            delete_info_from_dict(cpu_info)
-        elif choice == "3":
-            update_info_in_dict(cpu_info)
-        elif choice == "4":
+        print(f"Please choose operation to perform on {component_name}:")
+        operation_key, operation_name = _user_selection_from_dict(OPERATIONS_DICT)
+        print(operation_key)
+        if operation_key == "Add part":
+            add_info_to_dict(component_dict)
+        elif operation_key == "Delete part":
+            delete_info_from_dict(component_dict)
+        elif operation_key == "Update part":
+            update_info_in_dict(component_dict)
+        elif operation_key == "Exit":
             break
         else:
             print("Invalid choice. Please try again.")
 
     print("Final CPU Info Dictionary:")
-    print(cpu_info)
+    # print(cpu_info)
+
+
+def _user_selection_from_dict(the_dict):
+    """ Print the keys of the dictionary preceded by numbers 1..N, get selection from user, loop until successful. """
+    selected_value = None
+    while not selected_value:
+        count = 0
+        selection_dict = {}
+        for key, value in the_dict.items():
+            count += 1
+            print(f'{count}. {key}')
+            selection_dict[str(count)] = (str(key), value)
+        choice = input(f"Enter your choice (1-{count}): ")
+        selected_key, selected_value = selection_dict.get(choice)
+    return selected_key, selected_value
+
+
+if __name__ == "__main__":
+    main()
+
+
+
